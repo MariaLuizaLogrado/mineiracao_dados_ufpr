c6c788543181f22f6faebc0ae2f4601767bb6e28
Maria Luiza Logrado <logradomalu@gmail.com>
2024-10-23 20:31:46 -0300

    apontando o arquivo csv diretamente para o repositório do git

--- projeto/app.py
+++ projeto/app.py
@@ -25,7 +25,10 @@ with col2:
     st.image('https://www.rushu.rush.edu/sites/default/files/Research/worldhealthorg-who-logo.jpg', width=200)
 
 # Carregando o arquivo CSV
-df = pd.read_csv('./expectativa_vida.csv')
+
+url = "https://raw.githubusercontent.com/MariaLuizaLogrado/mineiracao_dados_ufpr/main/projeto/expectativa_vida.csv"
+df = pd.read_csv(url)
+
 mydf = df[['pais', 'ano', 'continente', 'relato_sarampo', 'expectativa_vida', 'consumo_alcool', 'mortalidade_adulta']]
 
 # Dicionário com limites geográficos aproximados para os continentes
