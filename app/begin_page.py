bacd132cd76fffe16985ac21a407c909bf697fad
Malu Logrado <maria.logrado@brf.com>
2024-12-02 11:42:41 -0300

    usando o caminho absoluto

--- app/begin_page.py
+++ app/begin_page.py
@@ -1,9 +1,9 @@
 import streamlit as st 
+import os
 from PIL import Image
 
-
-# image = Image.open("config/logo.png")
-# st.image(image, width=250)
+# Caminho absoluto baseado na localização do script
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 st.header("EasyOCR - Detecção de Texto - Text Detection")
 
@@ -13,16 +13,18 @@ ct = st.container()
 
 ct1, ct2 = ct.columns(2)
 
-comprador_button = ct1.button("Brasil", use_container_width=True)
-comprador_logo = Image.open("./config/br.png")
-ct1.image(comprador_logo, width=500)
+br_button = ct1.button("Brasil", use_container_width=True)
+image_br_path = os.path.join(base_dir, "config", "br.png")
+br_logo = Image.open(image_br_path)
+ct1.image(br_logo, width=500)
 
-coord_buton = ct2.button("United States", use_container_width = True)
-coord_logo = Image.open("./config/us.png")
-ct2.image(coord_logo, width=500)
+us_button = ct2.button("United States", use_container_width = True)
+image_us_path = os.path.join(base_dir, "config", "us.png")
+us_logo = Image.open(image_us_path)
+ct2.image(us_logo, width=500)
 
-if comprador_button:
+if br_button:
     st.switch_page("pt_ocr.py")
 
-if coord_buton:
+if us_button:
     st.switch_page("en_ocr.py")
